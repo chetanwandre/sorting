f0869e64779f263753e7667b7e5fc7372dcf420a
Chetan Wandre <164233886+chetanwandre@users.noreply.github.com>
2024-12-09 14:09:31 +0530

    Add files via upload

---
+++ selection_sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void selection_sort(int n , int arr[]){
+
+    //iteration through unsorted array
+    for(int i = 0; i<= n-2; i++){      //this main loop is for swapping purpose hence i will go from 0 to last ele ie n-2 only
+        int mini = i;         //lets assume that the very first ie it ele is mini ele    ,, its compared further with other elements in unsorted array
+
+        for(int j = i+1; j <= n-1; j++){       //this loop is only to find minimum element in the unsorted part of array
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+            //swapping is done out of j loop coz j loop is only to find mini ele   i loop is used for swapping 
+
+            //swapping 
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+   
+}
+
+int main(){
+    cout<<"enter the size "<<endl;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i< n; i++){
+        cin>>arr[i];
+    }
+
+    selection_sort(n, arr);
+
+    //print array after sorting 
+    for(int i = 0; i< n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
